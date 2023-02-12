7ff3adb371832ad5fee85f3afd04734ca031f8ee
rothy <rothy@lezhin.com>
2023-02-12 22:42:18 +0900

    RandomPlay for버전

--- src/main/java/basic/study/mission/RandomPlayList.java
+++ src/main/java/basic/study/mission/RandomPlayList.java
@@ -19,6 +19,7 @@ public class RandomPlayList {
             /* 람다의 캡쳐링이 일어나게 되면 값을 참조가 아닌 람다의 새로운 스택을 생성, 스택의 변경된 값을 반영할 방법이 없음
              * 그래서 재정의되지 않는 변수만 가능..
              */
+
             List<Integer> last3Temp = last3;
 
             List<String> filteredList = playList.stream()
@@ -29,6 +30,18 @@ public class RandomPlayList {
             System.out.println( i + "번째 음악! " + filteredList.get(randomMusic));
 
             last3 = Arrays.asList(playList.indexOf(filteredList.get(randomMusic)), last3.get(0), last3.get(1));
+
+/*            List<Integer> last4   = asList(-1,-1,-1);
+
+            for (;;) {
+                int ran = (int) (Math.random() * 10 + 1);
+                System.out.println("나오면 안되는 음악! : " + last4);
+                if (!last4.contains(ran)) {
+                    System.out.println(ran +" 음악~");
+                    last4 = asList(ran, last4.get(0), last4.get(1));
+                    break ;
+                }
+            }*/
         }
     }
 
