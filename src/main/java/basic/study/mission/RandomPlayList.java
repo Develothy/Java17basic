e4ff7ae34c8908d4691d6b7252c55f14aef68d50
rothy <rothy@lezhin.com>
2023-02-18 19:30:54 +0900

    playMusic 메소드

--- src/main/java/basic/study/mission/RandomPlayList.java
+++ src/main/java/basic/study/mission/RandomPlayList.java
@@ -1,48 +1,45 @@
 package basic.study.mission;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
-
-import static java.util.Arrays.asList;
+import java.util.*;
 
 public class RandomPlayList {
 
-
     public static void main(String[] args) {
-        List<String> playList = new ArrayList<>(asList("0.mp3","1.mp3","2.mp3","3.mp3","4.mp3","5.mp3","6.mp3","7.mp3","8.mp3","9.mp3"));
-        List<Integer> last3 = new ArrayList<>(asList(-1, -1, -1));
 
-        int playCnt = 20;
+        playMusic(10, 20);
+    }
+
+    static void playMusic(int playListSize, int playCnt) {
+        List<String> playList = makePlayList(playListSize);
+        List<Integer> last3 = new LinkedList<>();
 
         for(int i = 0; i < playCnt ; i++){
 
-            List<String> filteredList = playList.stream()
-                    .filter(it -> !last3.contains(playList.indexOf(it)))
-                    .collect(Collectors.toList());
-            System.out.println("나오면 안되는 음악! " + last3);
-            int randomMusic = (int)(Math.random() * filteredList.size());
-            System.out.println( i + "번째 음악! " + filteredList.get(randomMusic));
-
-            //last3 = Arrays.asList(playList.indexOf(filteredList.get(randomMusic)), last3.get(0), last3.get(1));
-            last3.remove(last3.size() -1);
-            last3.add(0, playList.indexOf(filteredList.get(randomMusic)));
-
-
-/*          for문을 이용한 방식
-
-            List<Integer> last4   = new ArrayList<>(asList(-1,-1,-1));
             for (;;) {
-                int ran = (int) (Math.random() * 10 + 1);
-                System.out.println("나오면 안되는 음악! : " + last4);
-                if (!last4.contains(ran)) {
-                    System.out.println(ran +" 음악~");
-                    last4.remove(last4.size() - 1);
-                    last4.add(0, ran);
+                int musicNum = (int) (Math.random() * 10);
+                System.out.println("나오면 안되는 음악! : " + last3);
+                if (!last3.contains(musicNum)) {
+                    System.out.println(playList.get(musicNum)+" ~ ♬");
+
+                    if (last3.size() == 3) {
+                        last3.remove(last3.size() - 1);
+                    }
+                    last3.add(0, musicNum);
+
                     break ;
                 }
             }
-*/
         }
     }
+
+    static List<String> makePlayList(int playListSize) {
+        List<String> playList = new ArrayList<>();
+
+        for (int i = 0; i < playListSize; i++) {
+            playList.add(i + ".mp3");
+        }
+
+        return playList;
+    }
+
 }
