6da3a80ddc006ad5cdd8e586563f91589e73ccce
rothy <rothy@lezhin.com>
2023-02-27 02:21:38 +0900

    print classLoader

--- src/main/java/basic/Basic17Application.java
+++ src/main/java/basic/Basic17Application.java
@@ -1,9 +1,13 @@
 package basic;
 
+import basic.study.chapter9.Car;
+import basic.study.mission.Cache;
+
 public class Basic17Application {
 
     public static void main(String[] args) {
-        System.out.println("start!");
+        System.out.println("Classloader of this class:"
+                + Basic17Application.class.getClassLoader());
     }
 
 }
