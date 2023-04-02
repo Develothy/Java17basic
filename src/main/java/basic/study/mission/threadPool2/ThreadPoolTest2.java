355598fd0c4751dc3947d89afc0e36b0a2a0334d
rothy <rothy@lezhin.com>
2023-04-02 20:47:34 +0900

    ThreadFactory test

---
+++ src/main/java/basic/study/mission/threadPool2/ThreadPoolTest2.java
@@ -0,0 +1,62 @@
+package basic.study.mission.threadPool2;
+
+import java.text.SimpleDateFormat;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
+
+import static basic.study.mission.threadPool2.ThreadPoolTest2.format;
+
+public class ThreadPoolTest2 {
+
+    final static SimpleDateFormat format = new SimpleDateFormat("hh:mm:sss:SSS");
+
+    static int count = 0;
+
+    public static void main(String[] args) {
+
+        // 새로운 쓰레드 추가시 로그 출력 ThreadFactory
+        ThreadFactory threadFactory = new ThreadFactory() {
+            @Override
+            public Thread newThread(Runnable r) {
+                Thread thread = new Thread(r);
+                System.out.println("Runnable"+"["+ count++ +"]"+" is created. [[ " + thread.getState() + " ]]");
+                return thread;
+            }
+        };
+
+        // 쓰레드 n개를 만드는 쓰레드풀 생성
+        ExecutorService p = Executors.newFixedThreadPool(10, threadFactory);
+        //ExecutorService p = Executors.newCachedThreadPool(threadFactory); // 4073개 out of memory ㅠ
+        //ExecutorService p = Executors.newScheduledThreadPool(10, threadFactory);
+
+        // TEST
+        try {
+            // item을 쓰레드풀에 담고 실행
+            for(int i = 0 ; i < 100000; i++){
+                Runnable r = new TestRunnable(i);
+                p.submit(r);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        p.shutdown();
+    }
+}
+
+class TestRunnable implements Runnable{
+
+    String name;
+    public TestRunnable(int i) {
+        name = "Thread" + i;
+    }
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(1000 * 3);
+            System.out.println("========= Run!! " + name + " : " + format.format(System.currentTimeMillis()) +" =========");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
